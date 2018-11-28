924335bdf84528156402d5591e1a651412530639
Matthew <mil26@scarletmail.rutgers.edu>
2018-11-28 14:06:01 -0500

    Created data.js

---
+++ data_includes/data.js
@@ -0,0 +1,24 @@
+/*
+-- WH judgement task
+-- Key for Items:
+    formalExpert
+    informalExpert -
+    filler1Informal - ne... pas/aucun fillers
+    filler2informalExpert -
+*/
+
+var shuffleSequence; // Work out randomization for items
+var practiceItemTypes; // Practice Items?
+var centerItems = true;
+
+
+var defaults = [
+
+];
+
+var items = [
+  /*
+  Do we need separators?
+  Is there a way to import from CSV?
+  */
+];
