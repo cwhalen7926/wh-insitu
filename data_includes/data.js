d155d7cd6858ed9f8ccc395c7fad76f8821c52b0
Matthew <mil26@scarletmail.rutgers.edu>
2018-12-03 10:54:25 -0500

    Update data.js with template for items

--- data_includes/data.js
+++ data_includes/data.js
@@ -7,18 +7,63 @@
     filler2informalExpert -
 */
 
-var shuffleSequence; // Work out randomization for items
+var shuffleSequence = seq(anytype); // Work out randomization for items
 var practiceItemTypes; // Practice Items?
 var centerItems = true;
 
 
 var defaults = [
-
+  "Question", {
+      //"as" option is obligatory
+      as: ["Yes", "No"],
+      hasCorrect: true
+        //if a question has a correct answer,
+          //keep it as the first element of the "as" option
+  }
 ];
 
 var items = [
   /*
   Do we need separators?
   Is there a way to import from CSV?
+  */
+
+  /*
+  INTRODUCTION
+  */
+
+  // TARGET ITEMS
+
+  /*
+  TEMPLATE for all items
+  ["target1_01", "Question", {hasCorrect: false, randomOrder: false, showNumbers: true,
+    q: "...",
+    as: [ "Option 1",
+          "Option 2"
+        ]}];
+  */
+
+  ["target1_01", "Question", {hasCorrect: false, randomOrder: false, showNumbers: true,
+    q: "...",
+    as: [ "Option 1",
+          "Option 2"
+        ]}];
+  /*
+  ["target1_02", "Question", {hasCorrect: false, randomOrder: false, showNumbers: true,
+    q: "...",
+    as: [ "Option 1",
+          "Option 2"
+        ]}];
+  ["target1_03", "Question", {hasCorrect: false, randomOrder: false, showNumbers: true,
+    q: "...",
+    as: [ "Option 1",
+          "Option 2"
+        ]}];
+
+  ["target1_04", "Question", {hasCorrect: false, randomOrder: false, showNumbers: true,
+    q: "...",
+    as: [ "Option 1",
+          "Option 2"
+        ]}];
   */
 ];
